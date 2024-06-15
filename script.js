5a84c08a3a3d029484677555a023fc915b752b42
vishakh <vishakhhrithi007@gmail.com>
2024-06-15 20:30:18 +0530

    set responsive

--- script.js
+++ script.js
@@ -5,23 +5,41 @@ let ul = document.getElementById("ul");
 function addlist() {
     let inputValue = input.value;
     let li = document.createElement("li");
-    li.textContent = inputValue;
     ul.appendChild(li);
+    let spanEl=document.createElement("span");
+    li.appendChild(spanEl);
+    spanEl.textContent = inputValue;
     input.value = "";
     input.focus();
     let trash = document.createElement("i");
-    trash.classList.add("fas", "fa-trash");
+    trash.classList.add("fas","fa-trash");
     li.appendChild(trash);
+    let edit = document.createElement("i");
+    edit.classList.add("fas","fa-edit");
+    li.appendChild(edit);
 }
 function deleteItem(event) {
-    if (event.target.classList[0] === "fas") {
+    if (event.target.classList[1] === "fa-trash") {
         let item = event.target.parentElement;
         item.classList.add("slideOut");
         item.addEventListener("transitionend", function () {
             item.remove();
         });
-    }
+    
+}
 }
 
+
+function editItem(event){
+if(event.target.classList[1]==="fa-edit"){
+let editedValue= prompt("please add new text ");
+let item = event.target.parentElement;
+
+let spanEl= item.querySelector("span");
+spanEl.innerText=  editedValue;
+
+}
+}
 btn.addEventListener("click", addlist);
 ul.addEventListener("click", deleteItem);
+ul.addEventListener("click",editItem);
